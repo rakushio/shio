9ff707d4ef2835dd282f339c9b9eb64c4000e9a5
rakushio <82456309+rakushio@users.noreply.github.com>
2021-04-14 14:14:56 +0800

    Add files via upload

---
+++ GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void StartGame() //新的游戏
+    {
+        Debug.Log("开始");
+    }
+
+    public void ContinueGame() //读取存档
+    {
+        Debug.Log("继续");
+    }
+
+    public void EndGame() //退出游戏
+    {
+        Debug.Log("结束");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
